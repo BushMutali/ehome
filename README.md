1fb9fba95777056122d97be29a14de1b9f5eb36e
BushMutali <102419698+BushMutali@users.noreply.github.com>
2024-08-21 13:00:24 +0300

    Readme, login and signup

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
+
+![MyImage](https://bushmutali.github.io/ehome/assets/img/icons/logo.png)
 ## E-Home Kenya
+
+Digital platform that enable one to search for homes and also property managers can register their properties.  
+
+# Custom bootstrap installation
+
+Install globally:
+```bash
+npm install -g sass
+npm install postcss-cli-autoprefixer
+```
+
+Navigate to your css folder. Create two files, `bootstrap.scss` and `bootstrap.css`
+
+In the css folder, open the terminal and run:
+
+```bash
+npm init
+# give it a name that is not 'bootstrap'
+# the rest of the prompt don't really matter you can just press enter
+```
+```bash
+npm install --save bootstrap
+```
+Now in your `bootstrap.scss` files you can change the value of the variables based on your liking. For example:
+
+```scss
+$primary: #004D40;
+$secondary: #FF6F61;
+$light: #F5F5f5;
+
+
+@import "node_modules/bootstrap/scss/bootstrap";
+```
+
+- NB: The last line is important. You need to have it.  
+- After that you can now run the following in your terminal.
+
+```bash
+sass bootstrap.scss bootstrap.css
+```
+
+> You have now successfully installed custom bootstrap theme.
+<span style="color: white">Anytime you make changes to the `bootstrap.scss` file make sure you run `sass bootstrap.scss bootstrap.css` in the terminal, in your css folder to save your new changes.</span>
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,91 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         spinner.classList.add('hidden');
     }, 1000);
+
+    // phone input validation
+    const phoneInput = document.getElementById('phone');
+    const feedback = document.getElementById('phoneFeedback');
+
+    phoneInput.addEventListener('input', function () {
+        let phoneValue = phoneInput.value.replace(/\D/g, '').slice(0, 12);
+        if (phoneValue.startsWith('0')) {
+            phoneValue = phoneValue.slice(1);
+        }
+        phoneInput.value = `${phoneValue}`;
+
+        if (phoneValue.length === 12) {
+            phoneInput.setCustomValidity('');
+            feedback.style.display = 'none';
+            phoneInput.classList.remove('is-invalid')
+        } else {
+            phoneInput.setCustomValidity('Invalid');
+            feedback.style.display = 'block';
+            phoneInput.classList.add('is-invalid')
+        }
+    });
+    phoneInput.addEventListener('focus', function () {
+        if (phoneInput.value === '') {
+            phoneInput.value = '+254';
+        }
+    });
+
+    phoneInput.addEventListener('blur', function () {
+        if (phoneInput.value === '+254') {
+            phoneInput.value = '';
+        }
+    });
+
+    // password validation
+    const passwordInput = document.getElementById('password');
+    const passfeedback = document.getElementById('passwordFeedback');
+
+    passwordInput.addEventListener('input', function () {
+        const passwordValue = passwordInput.value;
+        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,16}$/;
+
+        if (regex.test(passwordValue)) {
+            passwordInput.setCustomValidity('');
+            passfeedback.style.display = 'none';
+            passwordInput.classList.remove("is-invalid")
+        } else {
+            passwordInput.classList.add("is-invalid")
+            passwordInput.setCustomValidity('Invalid');
+            passfeedback.style.display = 'block';
+        }
+    });
+
+    // email validation
+    const emailInput = document.getElementById('email');
+    const emailfeedback = document.getElementById('emailFeedback');
+
+    emailInput.addEventListener('input', function () {
+        const emailValue = emailInput.value;
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (emailRegex.test(emailValue)) {
+            emailInput.setCustomValidity('');
+            emailInput.classList.remove("is-invalid")
+            emailfeedback.style.display = 'none';
+        } else {
+            emailInput.classList.add("is-invalid")
+            emailInput.setCustomValidity('Invalid');
+            emailfeedback.style.display = 'block';
+        }
+    });
+
+    // sub email validation 
+    const subEmail = document.getElementById('subEmail');
+    const subBtn = document.getElementById('subBtn')
+
+    subEmail.addEventListener('input', function () {
+        const subEmailValue = subEmail.value;
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (emailRegex.test(subEmailValue)) { 
+            subEmail.classList.remove('is-invalid');
+            subBtn.disabled = false
+        } else {
+            subEmail.classList.add('is-invalid')
+            subBtn.disabled = true
+        }
+    });
 });
